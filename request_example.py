68e0f5c9f6d4a9faab474404a2732382ceb5f56f
kuuuube <hexagonisalie@gmail.com>
2025-05-26 00:22:36 -0400

    Make more friendly to potential reading of developers as an example

--- request_example.py
+++ request_example.py
@@ -1,9 +1,29 @@
 import requests
 
-params = {
-    "term": "わかる",
-    "profileIndex": 0
-}
-response = requests.post("http://127.0.0.1:8766/termEntries", json = params)
-print(response)
-print(response.text)
+request_url = "http://127.0.0.1:8766"
+
+def termEntries():
+    print("Requesting termEntries:")
+    params = {
+        "term": "わかる",
+        "profileIndex": 0
+    }
+    response = requests.post(request_url + "/termEntries", json = params)
+    print(response)
+    print(str(response.json()["data"])[:100] + "...")
+
+def kanjiEntries():
+    print("Requesting kanjiEntries:")
+    params = {
+        "character": "分",
+        "profileIndex": 0
+    }
+    response = requests.post(request_url + "/kanjiEntries", json = params)
+    print(response)
+    print(str(response.json()["data"])[:100] + "...")
+
+print("Yomitan API request example demo")
+print("Only the first 100 characters of the result data for each request will be printed")
+print("--------------------------------------------------")
+termEntries()
+kanjiEntries()
