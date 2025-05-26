cc8e19deb9940da7a6a53504dee50c3c7848c5c3
kuuuube <hexagonisalie@gmail.com>
2025-05-26 14:00:03 -0400

    Request multiple handlebars in ankiFields

--- request_example.py
+++ request_example.py
@@ -30,7 +30,7 @@ def ankiFields():
     params = {
         "text": "わかる",
         "type": "term",
-        "handlebar": "glossary",
+        "handlebars": ["expression", "glossary"],
     }
     response = requests.post(request_url + "/ankiFields", json = params)
     print(response)
