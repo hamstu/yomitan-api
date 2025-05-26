e46bc71f37a59560af948eba852b52dcba93295e
kuuuube <hexagonisalie@gmail.com>
2025-05-26 00:08:53 -0400

    Simplify user facing api params

--- request_example.py
+++ request_example.py
@@ -1,10 +1,9 @@
 import requests
 
 params = {
-    "text": "わかる",
-    "details": {},
-    "optionsContext": {"index": 0}
+    "term": "わかる",
+    "profileIndex": 0
 }
-response = requests.post("http://127.0.0.1:8766/termsFind", json = params)
+response = requests.post("http://127.0.0.1:8766/termEntries", json = params)
 print(response)
 print(response.text)
