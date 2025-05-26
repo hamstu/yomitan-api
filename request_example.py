82ad44744946f1373d68b6e087d509d89e8a6dd2
kuuuube <hexagonisalie@gmail.com>
2025-05-26 14:11:16 -0400

    Request all default markers and add kanji ankiFields request type

--- request_example.py
+++ request_example.py
@@ -25,12 +25,23 @@ def kanjiEntries():
     print(response)
     print(elide(str(response.json()["data"])))
 
-def ankiFields():
-    print("Requesting ankiFields:")
+def ankiFields_term():
+    print("Requesting ankiFields type term:")
     params = {
         "text": "わかる",
         "type": "term",
-        "markers": ["expression", "glossary"],
+        "markers": ["audio", "cloze-body-kana", "conjugation", "expression", "furigana", "furigana-plain", "glossary", "glossary-brief", "glossary-no-dictionary", "glossary-first", "glossary-first-brief", "glossary-first-no-dictionary", "part-of-speech", "phonetic-transcriptions", "pitch-accents", "pitch-accent-graphs", "pitch-accent-graphs-jj", "pitch-accent-positions", "pitch-accent-categories", "reading", "tags", "clipboard-image", "clipboard-text", "cloze-body", "cloze-prefix", "cloze-suffix", "dictionary", "dictionary-alias", "document-title", "frequencies", "frequency-harmonic-rank", "frequency-harmonic-occurrence", "frequency-average-rank", "frequency-average-occurrence", "screenshot", "search-query", "popup-selection-text", "sentence", "sentence-furigana", "sentence-furigana-plain", "url"],
+    }
+    response = requests.post(request_url + "/ankiFields", json = params)
+    print(response)
+    print(elide(str(response.json()["data"])))
+
+def ankiFields_kanji():
+    print("Requesting ankiFields type kanji:")
+    params = {
+        "text": "分",
+        "type": "kanji",
+        "markers": ["character", "glossary", "kunyomi", "onyomi", "onyomi-hiragana", "stroke-count", "clipboard-image", "clipboard-text", "cloze-body", "cloze-prefix", "cloze-suffix", "dictionary", "dictionary-alias", "document-title", "frequencies", "frequency-harmonic-rank", "frequency-harmonic-occurrence", "frequency-average-rank", "frequency-average-occurrence", "screenshot", "search-query", "popup-selection-text", "sentence", "sentence-furigana", "sentence-furigana-plain", "url"],
     }
     response = requests.post(request_url + "/ankiFields", json = params)
     print(response)
@@ -41,4 +52,5 @@ print("Only the first 100 characters of the result data for each request will be
 print("--------------------------------------------------")
 termEntries()
 kanjiEntries()
-ankiFields()
+ankiFields_term()
+ankiFields_kanji()
